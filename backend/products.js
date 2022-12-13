9aa71e25a83cd580cb51c0fc690b4f1c401a2555
Kyu0z <duykhaidevfe@gmail.com>
2022-12-13 14:15:10 +0700

    Upload 13/12

---
+++ backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 50,
+    image: "./iphone12pro.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 60,
+    image: "./iphone12.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 20,
+    image: "./galaxyS.png",
+  },
+];
+
+module.exports = products;
